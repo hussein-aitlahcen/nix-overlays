82f655dffb3e27f016a8a3979f5e11fe93252642
= <hussein.aitlahcen@gmail.com>
2018-02-01 23:24:58 +0100

    fix(emacs): override packageNg scope

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,11 @@
 final: previous: 
 
 let 
-	inherit (final) callPackage;
+	inherit (final) callPackage newScope;
 in 
 {
 	emacs = callPackage /.emacs { };
+	emacsPackagesNg = previous.emacsPackagesNg.overrideScope (_: self: {
+		inherit (final) emacs;
+	});
 }
