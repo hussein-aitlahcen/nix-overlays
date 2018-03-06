6a7648b7ad1d4de51564292ca1b4fcc695c5b141
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-03-06 10:27:15 +0100

    fix: processing3

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,12 +4,12 @@ let
 	inherit (final) callPackage newScope;
 in
 {
+	processing3 = callPackage ./processing { };
+
   zsh-theme-dracula = callPackage ./zsh-theme-dracula { };
 
 	emacs = callPackage ./emacs { };
 	emacsPackagesNg = previous.emacsPackagesNg.overrideScope (_: self: {
 		inherit (final) emacs;
 	});
-
-	processing3 = callPackage ./processing { };
 }
