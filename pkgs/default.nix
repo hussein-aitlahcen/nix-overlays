14df4ecaf4622353561640e5716cf5ed20d8f269
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-03-23 19:26:16 +0100

    fix: desperate

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,7 @@
 self: super:
 
-let
-	inherit (final) callPackage;
-in
 {
-	processing3 = callPackage ./processing { };
-
-  zsh-theme-dracula = callPackage ./zsh-theme-dracula { };
-
-	emacs26 = callPackage ./emacs { };
+	processing3       = super.callPackage ./processing { };
+  zsh-theme-dracula = super.callPackage ./zsh-theme-dracula { };
+	emacs26           = super.callPackage ./emacs { };
 }
